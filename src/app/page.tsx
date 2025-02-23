038c1bdc34f107d804c88cdccb7a948b99157e63
Debendra Meher <dmeher1996@gmail.com>
2025-02-23 12:35:30 +0530

    Added auto redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  setTimeout(() => {
+    redirect("/login");
+  }, 2000);
+
   return (
     <main>
-      <div>Hello world!</div>
+      <div>You will be redirected to login page in 2 seconds.</div>
     </main>
   );
 }
